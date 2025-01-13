69a0bb6ed723b1f309e253dea57b3d68c9ccd652
djoandjo <joaquim.nicolas94@gmail.com>
2025-01-13 14:37:37 +0100

    ajout de login token

--- quiz-api/app.py
+++ quiz-api/app.py
@@ -1,5 +1,7 @@
-from flask import Flask
+from flask import Flask , request
 from flask_cors import CORS
+import hashlib
+from jwt_utils import decode_token,build_token
 
 app = Flask(__name__)
 
@@ -10,7 +12,34 @@ CORS(app)
 def hello_world():
 	x = 'guys'
 	return f"Hello, {x}"
-  
+
+@app.route('/login', methods=['POST'])
+def login():
+
+        # Récupération des données JSON
+        data = request.json
+        if not data:
+            return 'Unauthorized', 401
+        
+        password = data.get('password')
+
+        # Validation des paramètres
+        if  not password:
+            return 'Unauthorized', 401
+
+        # Vérification des identifiants
+        try_psw = password.encode('UTF-8')
+        hash = hashlib.md5(try_psw).digest()
+
+        if hash == b'\xd8\x17\x06PG\x92\x93\xc1.\x02\x01\xe5\xfd\xf4_@':
+            token = build_token()
+
+            "Good Password "
+            return { "message": "Login successful","token": token},200
+        else:
+            return {"message" :'Unauthorized' }, 401
+
+
 
 @app.route('/quiz-info', methods=['GET'])
 def GetQuizInfo():
--- quiz-api/venv/Scripts/activate.bat
+++ quiz-api/venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\joaqu\OneDrive\Bureau\esiee\itE5\quiz-app\quiz-api\venv
+set VIRTUAL_ENV=C:\Users\joaqu\OneDrive\Bureau\esiee\e5\itE5\quiz-app\quiz-api\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
