84a026a6c37c32731f47988622eb02e2ff57a7da
YatoLyaris <jules.buv02@orange.fr>
2025-01-13 19:02:19 +0100

    partie 2 front

---
+++ quiz-ui/src/views/HomePage.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { ref, onMounted } from 'vue';
+import quizApiService from "@/services/QuizApiService";
+
+const registeredScores = ref([]);
+
+onMounted(async () => {
+		console.log("Home page mounted");
+});
+</script>
+
+<style>
+</style>
+
+<template>
+  <h1>Home page</h1>
+
+  <div v-for="scoreEntry in registeredScores" v-bind:key="scoreEntry.date">
+    {{ scoreEntry.playerName }} - {{ scoreEntry.score }}
+  </div>
+
+  <router-link to="/new-quiz">Démarrer le quiz !</router-link>
+</template>
